4df36cc3b9dfe00e0840c47c2863d5533aab2e10
nihaals863-lgtm <nihaals863@gmail.com>
2026-03-24 16:59:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(cors({
       process.env.CLIENT_URL,
       'http://localhost:5173',
       'http://localhost:5174',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173',
+      'https://loan-managment-24-03.netlify.app'
     ].filter(Boolean);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
